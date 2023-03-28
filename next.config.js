6e24bfb4be6cafe5858282926ed6475b9008ae74
Lucao Adam <lucoadam@gmail.com>
2023-03-28 12:40:55 +0545

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache.js');
+const isProduction = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+const config = {
+  // here goes your Next.js configuration
+};
+
+const nextConfig = withPWA({
+  dest: 'public',
+  disable: !isProduction,
+  runtimeCaching
+})(
+  config
+);
+
+module.exports = nextConfig;
